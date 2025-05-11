5f04b4aa6eb1244e9391356233277534e21e20f7
loarsaw <amanahmed1385@gmail.com>
2025-05-11 20:43:19 +0530

    fix

--- backend/src/controller/auth/sign-in/signin.ts
+++ backend/src/controller/auth/sign-in/signin.ts
@@ -6,45 +6,52 @@ import { getMongoDBInstance } from "../../../config/mongodb";
 const JWT_SECRET = process.env.JWT_SECRET ?? "";
 
 export const signin = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ message: "Missing credentials" });
+        if (!email || !password) {
+            res.status(400).json({ message: "Missing credentials" });
+            return
+        }
+
+        const db = await getMongoDBInstance();
+        if (!db) {
+            res.status(500).json({ message: "DB connection failed" });
+
+            return
+        }
+
+        const privateCollection = db.collection("private");
+
+        const user = await privateCollection.findOne({ email });
+        if (!user) {
+            res.status(401).json({ message: "Invalid email or password" });
+            return
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(401).json({ message: "Invalid email or password" });
+            return
+        }
+
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        const { password: _password, ...userWithoutPassword } = user;
+
+        res.status(200).json({
+            message: "Login successful",
+            user: userWithoutPassword,
+            token,
+        });
+        return
+    } catch (err) {
+        console.error("Signin Error:", err);
+        res.status(500).json({ message: "Internal Server Error" });
+        return
     }
-
-    const db = await getMongoDBInstance();
-    if (!db) {
-      return res.status(500).json({ message: "DB connection failed" });
-    }
-
-    const privateCollection = db.collection("private");
-
-    const user = await privateCollection.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-
-    const token = jwt.sign(
-      { id: user._id, email: user.email },
-      JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    const { password: _password, ...userWithoutPassword } = user;
-
-    return res.status(200).json({
-      message: "Login successful",
-      user: userWithoutPassword,
-      token,
-    });
-  } catch (err) {
-    console.error("Signin Error:", err);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
 };

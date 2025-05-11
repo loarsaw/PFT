bd4a2295eee6bfc99bece63118b505637ce1a151
loarsaw <amanahmed1385@gmail.com>
2025-05-11 20:33:35 +0530

    sending bearer token

--- client/utils/axiosInstance.ts
+++ client/utils/axiosInstance.ts
@@ -1,11 +1,26 @@
+import { store } from "@/redux/store";
 import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://closing-dinosaur-apt.ngrok-free.app/api",
-  timeout: 10000,
+    timeout: 10000,
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "asklaksl993",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state.user.token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
